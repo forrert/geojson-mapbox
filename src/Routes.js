564c898782959e4f7aeeb6ed9e8f4d0931ea7079
Thomas Forrer <forrert@gmail.com>
2018-03-20 21:09:41 -0700

    set up for publishing to github pages

--- src/Routes.js
+++ src/Routes.js
@@ -4,9 +4,14 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import DataSources from "./pages/DataSources";
 import DataSoureDetail from "./pages/DataSources/DataSourceDetail";
 
+let basename = "";
+if (process.env.NODE_ENV === "production") {
+    basename = "geojson-mapbox";
+}
+
 const Routes = () => {
     return (
-        <Router>
+        <Router basename={basename}>
             <div>
                 <Route exact path="/" component={DataSources} />
                 <Route path="/DataSources/:id" component={DataSoureDetail} />
